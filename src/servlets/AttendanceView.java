a23000a91b7e50d9d760e7c88117ac526011dc60
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2018-01-06 08:40:29 -0500

    attendance view added

--- src/servlets/AttendanceView.java
+++ src/servlets/AttendanceView.java
@@ -1,11 +1,19 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import helpers.AttendanceHelper;
+import helpers.EmployeeHelper;
+import helpers.ReportHelper;
+import objects.Employee;
 
 /**
  * Servlet implementation class AttendanceView
@@ -26,7 +34,8 @@ public class AttendanceView extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Map<String,String> departments = ReportHelper.getDepartmentList();
+		request.setAttribute("departments", departments);
 		request.getRequestDispatcher("/WEB-INF/jsp/employee-attendance/attendance_view.jsp").forward(request, response);
 	}
 
@@ -34,8 +43,18 @@ public class AttendanceView extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Map<String,String> departments = ReportHelper.getDepartmentList();
+		request.setAttribute("departments", departments);
+		
+		String departmentId = request.getParameter("department");
+		Integer depID = Integer.parseInt(request.getParameter("department"));
+		ArrayList<Employee> employee = EmployeeHelper.getEmployees(departmentId);
+		ArrayList<String> attenDates = AttendanceHelper.getAttendanceDates(depID);
+		ArrayList<ArrayList<String>> attendanceCheck = AttendanceHelper.getEmployeeAttendance(depID, employee);
+		request.setAttribute("attenCheck", attendanceCheck);
+		request.setAttribute("attenDates", attenDates);
+		request.setAttribute("employeeL", employee);
+		request.getRequestDispatcher("/WEB-INF/jsp/employee-attendance/attendance_view.jsp").forward(request, response);
 	}
 
 }
